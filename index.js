3d21b3b731e656b3c07a20d5bcc43de0ca19f936
devsona <122227306+devsona@users.noreply.github.com>
2023-03-02 16:34:16 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function mapDisplay() {
 function exitDispaly(){
     document.querySelector('.maps-holder').style.display = 'none';
 };
+
+let dateObj = new Date();
+let month = dateObj.getUTCMonth() + 1;
+let day = dateObj.getDay() - 2;
+let year = dateObj.getUTCFullYear();
+newdate = month + "/" + day + "/" + year;
+
+let elDay = document.getElementById('day');
+elDay.innerHTML = 'Today is: ' + newdate;
+function openOrClose() {
+    if (day == 7){
+    let close = document.getElementById('open').innerHTML = "sorry we are closed :C, come back tommow!"
+    return 'close'
+    } else{
+    let close = document.getElementById('open').innerHTML = "we're open Today! please check hours for more information" 
+    return 'open'
+    }
+}
+console.log(openOrClose())
